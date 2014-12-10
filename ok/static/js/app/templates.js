3100957057b365382faab34bda528dc50d14aa5a
Christophe-Marie Duquesne <chmd@chmd.fr>
2014-12-10 22:53:25 +0100

    reformatting

--- ok/static/js/app/templates.js
+++ ok/static/js/app/templates.js
@@ -2,29 +2,25 @@
 
 define(
   function() {
-    var breadcrumbs='{{#path}}<li><a href="{{target}}">{{component}}</a></li>{{/path}}';
-    var users='                  \
-    {{#users}}                   \
-      <tr>                       \
-          <td>{{username}}</td>  \
-          <td>                   \
-          {{#groups}}            \
-          {{/groups}}            \
-          </td>                  \
-      </tr>                      \
-    {{/users}}                   \
-      <tr>  \
-          <td>2</td>  \
-          <td>Jacob</td>  \
-          <td>Thornton</td>  \
-      </tr>  \
-      <tr>  \
-          <td>3</td>  \
-          <td>Larry</td>  \
-          <td>the  \
-              Bird</td>  \
-      </tr>  \
-  \
+    var breadcrumbs='                        \
+    {{#path}}                                \
+    <li>                                     \
+      <a href="{{target}}">{{component}}</a> \
+    </li>                                    \
+    {{/path}}';
+    var users='                 \
+    {{#users}}                  \
+      <tr>                      \
+          <td>{{username}}</td> \
+          <td>                  \
+          {{#groups}}           \
+          {{#first}}            \
+          {{groupname}}         \
+          {{/first}}            \
+          ,{{groupname}}        \
+          {{/groups}}           \
+          </td>                 \
+      </tr>                     \
     ';
     return {
       breadcrumbs: breadcrumbs
